762ba57df323984f822dcf410348f508e27e2d5d
mricher-git <65866023+mricher-git@users.noreply.github.com>
2024-03-14 01:37:30 +0800

    Add double-click and shift-select lead loco behavior

--- HudEnhancer/HudEnhancer.cs
+++ HudEnhancer/HudEnhancer.cs
@@ -4,6 +4,7 @@ using Game.State;
 using HarmonyLib;
 using HudEnhancer.UMM;
 using Model;
+using RollingStock;
 using System;
 using System.Linq;
 using UI;
@@ -11,101 +12,148 @@ using UI.CarInspector;
 using UI.Common;
 using UnityEngine;
 
-namespace HudEnhancer;
-
-public class HudEnhancer : MonoBehaviour
+namespace HudEnhancer
 {
-	public enum MapStates { MAINMENU, MAPLOADED, MAPUNLOADING }
-	public static MapStates MapState { get; private set; } = MapStates.MAINMENU;
-	internal Loader.HudEnhancerSettings Settings;
 
-	private AIControls aiControls;
-
-	public static HudEnhancer Instance
+	public class HudEnhancer : MonoBehaviour
 	{
-		get { return Loader.Instance; }
-	}
+		public enum MapStates { MAINMENU, MAPLOADED, MAPUNLOADING }
+		public static MapStates MapState { get; private set; } = MapStates.MAINMENU;
+		internal Loader.HudEnhancerSettings Settings;
 
-	void Start()
-	{
-		Messenger.Default.Register<MapDidLoadEvent>(this, new Action<MapDidLoadEvent>(this.OnMapDidLoad));
-		Messenger.Default.Register<MapWillUnloadEvent>(this, new Action<MapWillUnloadEvent>(this.OnMapWillUnload));
+		private AIControls aiControls;
 
-		if (StateManager.Shared.Storage != null)
+		public static HudEnhancer Instance
 		{
-			OnMapDidLoad(new MapDidLoadEvent());
-		}
-	}
-
-	private void OnMapDidLoad(MapDidLoadEvent evt)
-	{
-		Loader.LogDebug("OnMapDidLoad");
-		if (MapState == MapStates.MAPLOADED) return;
-		MapState = MapStates.MAPLOADED;
-		
-		aiControls = AIControls.Create();
-
-		//Messenger.Default.Register<WorldDidMoveEvent>(this, new Action<WorldDidMoveEvent>(this.WorldDidMove));
-	}
-
-	private void OnMapWillUnload(MapWillUnloadEvent evt)
-	{
-		Loader.LogDebug("OnMapWillUnload");
-
-		MapState = MapStates.MAPUNLOADING;
-		//Messenger.Default.Unregister<WorldDidMoveEvent>(this);
-	}
-
-	/*(private void WorldDidMove(WorldDidMoveEvent evt)
-	{
-		Loader.LogDebug("WorldDidMove");
-	}*/
-
-	void OnDestroy()
-	{
-		Loader.LogDebug("OnDestroy");
-
-		Messenger.Default.Unregister<MapDidLoadEvent>(this);
-		Messenger.Default.Unregister<MapWillUnloadEvent>(this);
-
-		if (MapState == MapStates.MAPLOADED)
-		{
-			OnMapWillUnload(new MapWillUnloadEvent());
-
-			if (aiControls != null) DestroyImmediate(aiControls);
+			get { return Loader.Instance; }
 		}
 
-		MapState = MapStates.MAINMENU;
-	}
+		void Start()
+		{
+			Messenger.Default.Register<MapDidLoadEvent>(this, new Action<MapDidLoadEvent>(this.OnMapDidLoad));
+			Messenger.Default.Register<MapWillUnloadEvent>(this, new Action<MapWillUnloadEvent>(this.OnMapWillUnload));
 
-	public void OnSettingsChanged()
-	{
-		if (MapState != MapStates.MAPLOADED) return;
+			if (StateManager.Shared.Storage != null)
+			{
+				OnMapDidLoad(new MapDidLoadEvent());
+			}
+		}
+
+		private void OnMapDidLoad(MapDidLoadEvent evt)
+		{
+			Loader.LogDebug("OnMapDidLoad");
+			if (MapState == MapStates.MAPLOADED) return;
+			MapState = MapStates.MAPLOADED;
+
+			aiControls = AIControls.Create();
+
+			//Messenger.Default.Register<WorldDidMoveEvent>(this, new Action<WorldDidMoveEvent>(this.WorldDidMove));
+		}
+
+		private void OnMapWillUnload(MapWillUnloadEvent evt)
+		{
+			Loader.LogDebug("OnMapWillUnload");
+
+			MapState = MapStates.MAPUNLOADING;
+			//Messenger.Default.Unregister<WorldDidMoveEvent>(this);
+		}
+
+		/*(private void WorldDidMove(WorldDidMoveEvent evt)
+		{
+			Loader.LogDebug("WorldDidMove");
+		}*/
+
+		void OnDestroy()
+		{
+			Loader.LogDebug("OnDestroy");
+
+			Messenger.Default.Unregister<MapDidLoadEvent>(this);
+			Messenger.Default.Unregister<MapWillUnloadEvent>(this);
+
+			if (MapState == MapStates.MAPLOADED)
+			{
+				OnMapWillUnload(new MapWillUnloadEvent());
+
+				if (aiControls != null) DestroyImmediate(aiControls);
+			}
+
+			MapState = MapStates.MAINMENU;
+		}
+
+		public void OnSettingsChanged()
+		{
+			if (MapState != MapStates.MAPLOADED) return;
+		}
 	}
 }
 
-[HarmonyPatch(typeof(CarInspector), nameof(CarInspector.SelectConsist))]
- static class SelectConsistPatch
+namespace HudEnhancer.Patches
 {
-	public static bool Prefix(Car ____car, Window ____window)
-	{
-		if (GameInput.IsControlDown)
-		{
-			if (!____car.IsLocomotive)
-			{
-				var car = ____car.EnumerateCoupled(Car.LogicalEnd.A).First();
-				if (car != null && car.IsLocomotive)
-				{
-					TrainController.Shared.SelectedCar = car;
 
+	[HarmonyPatch(typeof(CarInspector), nameof(CarInspector.SelectConsist))]
+	static class SelectConsistPatch
+	{
+		public static bool Prefix(Car ____car, Window ____window)
+		{
+			if (GameInput.IsShiftDown)
+			{
+				if (!____car.IsLocomotive)
+				{
+					var loco = ____car.FindLeadLoco();
+
+					if (loco != null)
+					{
+						TrainController.Shared.SelectedCar = loco;
+
+						return false;
+					}
+				}
+				else
+				{
+					TrainController.Shared.SelectedCar = ____car;
+				}
+				return false;
+			}
+			return true;
+		}
+	}
+
+	[HarmonyPatch(typeof(CarPickable), nameof(CarPickable.Activate))]
+	static class CarPickableDoubleClickPatch
+	{
+		private static float lastClick = 0f;
+		private static bool Prefix(Car ___car)
+		{
+			if (!GameInput.IsControlDown)
+			{
+				if (Time.unscaledTime - lastClick < 0.25f)
+				{
 					if (GameInput.IsShiftDown)
 					{
-						____window.CloseWindow();
+						var loco = ___car.FindLeadLoco();
+						___car = loco != null ? loco : ___car;
 					}
+
+					TrainController.Shared.SelectedCar = ___car;
 					return false;
 				}
 			}
+
+			lastClick = Time.unscaledTime;
+
+			return true;
 		}
-		return true;
+	}
+
+	public static class CarExtensions
+	{
+		public static BaseLocomotive? FindLeadLoco(this Car car)
+		{
+			var loco = car.EnumerateCoupled(Car.LogicalEnd.A).First() as BaseLocomotive;
+			if (loco == null)
+				loco = car.EnumerateCoupled(Car.LogicalEnd.B).First() as BaseLocomotive;
+
+			return loco;
+		}
 	}
 }
